b0967f97e3fb9138ccfcb53f69c6c065f4054486
apachaitime <apachaitime@gmail.com>
2026-02-05 15:58:01 -0600

    Updated env variables

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 const mongoose = require("mongoose");
 const { NOT_FOUND_ERROR } = require("./utils/errors");
 
-const { PORT = 3001 } = process.env;
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+const { PORT = 3001, MONGODB_URI =
+"mongodb://127.0.0.1:27017/wtwr_db"} = process.env;
+mongoose.connect(MONGODB_URI)
 app.use(express.json());
 
 const clothingItemsRoutes = require("./routes/clothingItems");
